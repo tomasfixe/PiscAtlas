e4cd0aa5b85313e7f8b24ba52cdb1fc51c125a5d
antoniobmatos <ambmatos193@gmail.com>
2026-03-29 21:45:43 +0100

    Novas páginas e controladore criados

---
+++ PiscAtlas.WebApp/Controllers/CapturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PiscAtlas.WebApp.Controllers
+{
+    public class CapturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PiscAtlas.WebApp/Controllers/EventoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PiscAtlas.WebApp.Controllers
+{
+    public class EventoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
