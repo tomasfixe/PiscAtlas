dbed0ec671e06592ad4f2f9d3b48ab83f586cd5a
tomasfixe <tomasfaria2004@gmail.com>
2026-03-29 20:24:44 +0200

    Classe Utilizador

---
+++ PiscAtals.Models/Models/Utilizador.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PiscAtlas.Models.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PiscAtlas.Models.Models
+{
+    public class Utilizador : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string PrimeiroNome { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UltimoNome { get; set; }
+
+        public string FotografiaPerfilUrl { get; set; }
+        public DateTime DataRegisto { get; set; } = DateTime.Now;
+        public string NomeCompleto => $"{PrimeiroNome} {UltimoNome}";
+
+        public virtual ICollection<Captura> Capturas { get; set; }
+        public virtual ICollection<Inscricao> Inscricoes { get; set; }
+    }
+}
