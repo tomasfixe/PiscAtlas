3ff86b0a9a82178a5196f8de079da191150b69d4
antoniobmatos <ambmatos193@gmail.com>
2026-03-29 19:54:36 +0100

    EspecieControler + Index de espécies + layout

---
+++ PiscAtlas.WebApp/Controllers/EspecieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PiscAtlas.Models;
+
+namespace PiscAtlas.WebApp.Controllers
+{
+    public class EspecieController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Injetamos a base de dados no controlador
+        public EspecieController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Ação que gera a página "Caderneta"
+        public async Task<IActionResult> Index()
+        {
+            // Vai buscar todas as espécies à base de dados
+            var especies = await _context.Especies.ToListAsync();
+            return View(especies);
+        }
+    }
+}
