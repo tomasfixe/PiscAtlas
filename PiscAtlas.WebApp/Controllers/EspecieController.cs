0cb6055bea23895e7bf3bc7f4fe7729a1aea412e
tomasfixe <tomasfaria2004@gmail.com>
2026-06-26 21:09:25 +0100

    Função para admin adicionar novas espécies

--- PiscAtlas.WebApp/Controllers/EspecieController.cs
+++ PiscAtlas.WebApp/Controllers/EspecieController.cs
@@ -25,17 +25,17 @@ namespace PiscAtlas.WebApp.Controllers
         {
             var userId = _userManager.GetUserId(User);
 
-            //  Vai buscar todas as espécies
+            // Vai buscar todas as espécies
             var todasEspecies = await _context.Especies.OrderBy(e => e.Nome).ToListAsync();
 
-            //  Vai buscar os IDs das espécies que este utilizador já apanhou
+            // Vai buscar os IDs das espécies que este utilizador já apanhou
             var especiesCapturadasIds = await _context.Capturas
                 .Where(c => c.UtilizadorId == userId)
                 .Select(c => c.EspecieId)
-                .Distinct() // Garante que não conta o duas vezes
+                .Distinct() // Garante que não conta o mesmo peixe duas vezes
                 .ToListAsync();
 
-            // 3. Empacota tudo para enviar para o Ecrã
+            // Empacota tudo para enviar para o Ecrã
             var vm = new CadernetaViewModel
             {
                 TodasEspecies = todasEspecies,
@@ -43,6 +43,48 @@ namespace PiscAtlas.WebApp.Controllers
             };
 
             return View(vm);
+        }
+
+        
+
+        // GET: Especie/Criar (Apenas Admin)
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        // POST: Especie/Criar (Apenas Admin)
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Criar(Especie model)
+        {
+            ModelState.Remove("PesoRecordPt");
+            ModelState.Remove("TamanhoRecordPt");
+            ModelState.Remove("Capturas");
+            ModelState.Remove("ImagemUrl");
+
+            if (ModelState.IsValid)
+            {
+                // Proteção caso os campos venham vazios
+                model.Descricao = model.Descricao ?? "";
+                // Atribuir um valor vazio à imagem para não dar erro na base de dados
+                model.ImagemUrl = "";
+
+                //Para ao criar espécies são seja preciso meter um recorde
+                model.PesoRecordPt = 0;
+                model.TamanhoRecordPt = 0;
+
+                _context.Especies.Add(model);
+                await _context.SaveChangesAsync();
+
+                TempData["Sucesso"] = "Nova espécie adicionada com sucesso à caderneta!";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
         }
     }
 
@@ -56,6 +98,6 @@ namespace PiscAtlas.WebApp.Controllers
         public int TotalEspecies => TodasEspecies.Count;
         public int PorFazer => TotalEspecies - TotalCapturadas;
         public int Percentagem => TotalEspecies == 0 ? 0 : (int)Math.Round((double)TotalCapturadas / TotalEspecies * 100);
-        public int Pontos => TotalCapturadas * 100; // 100 pontos por cada espécie diferente!
+        public int Pontos => TotalCapturadas * 100; // 100 pontos por cada espécie diferente
     }
 }
